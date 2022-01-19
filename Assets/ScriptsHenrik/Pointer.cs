49796de67729b2b19563316826226d6cf89d6659
Henrik <henrik.sedvall@gmail.com>
2022-01-19 09:06:06 +0100

    Add some pointer functionalities, like change color on object when hoovering and logging stats

---
+++ Assets/ScriptsHenrik/Pointer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    //When the mouse hovers over the GameObject, it turns to this color (red)
+    Color m_MouseOverColor;
+
+    //This stores the GameObject’s original color
+    Color m_OriginalColor;
+
+    //Get the GameObject’s mesh renderer to access the GameObject’s material and color
+    MeshRenderer m_Renderer;
+
+    bool hoover = true;
+
+    void Start()
+    {
+        //Fetch the mesh renderer component from the GameObject
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_MouseOverColor = new Color(1,0,0.7f,0.5f);
+        //Fetch the original color of the GameObject
+        m_OriginalColor = m_Renderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        // Change the color of the GameObject to red when the mouse is over GameObject
+        m_Renderer.material.color = m_MouseOverColor;
+        
+        if (hoover)
+        {
+            Debug.Log("Baby need 1: Food");
+            Debug.Log("Baby need 2: Diaper change");
+            Debug.Log("Baby need 3: Sleep");
+            hoover = false;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        hoover = true;
+        // Reset the color of the GameObject back to normal
+        m_Renderer.material.color = m_OriginalColor;
+    }
+}
