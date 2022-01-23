f1d1ed56b7ebeb047a59908aaae329c66472cb68
Henrik <henrik.sedvall@gmail.com>
2022-01-23 23:24:58 +0100

    Fix movement based on feedback from mid review. Adding rotation (WIP), and began on grabbing/moving functionality (WIP)

--- Assets/Scripts/ScriptsHenrik/CameraController.cs
+++ Assets/Scripts/ScriptsHenrik/CameraController.cs
@@ -7,12 +7,55 @@ public class CameraController : MonoBehaviour
 {
     public Transform playerTransform;
     [SerializeField] private float smoothSpeed = 3f;
-    [SerializeField] private Vector3 offset = new Vector3( 0, 23, -9);
+    [SerializeField] private float sweepSmoothSpeed = 10f;
+    [SerializeField] private Vector3 offset = new Vector3(0, 23, -9);
+    [SerializeField] private Vector3 sweepOffset = new Vector3(0, 8, -3);
+    [SerializeField] private Vector3 sweepRotation = new Vector3(0, 8, -3);
 
+    
     void LateUpdate() 
+    {
+        if(Input.GetKey(KeyCode.C))
+        {
+            CameraSweep();
+        }
+        else CameraFollowPlayer();
+    }
+
+    void CameraFollowPlayer()
     {
         Vector3 desiredPosition = new Vector3(playerTransform.position.x, 0, 0) + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
+    }
+    void CameraSweep()
+    {
+        SetCameraDirection();
+        // Positions
+        Vector3 desiredPosition = playerTransform.position + sweepOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, sweepSmoothSpeed * Time.deltaTime);
+        // Rotations
+        
+
+        transform.position = smoothedPosition;
+        Debug.Log(smoothedPosition);
+    }
+
+    void SetCameraDirection()
+    {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = playerTransform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = sweepSmoothSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
--- Assets/Scripts/ScriptsHenrik/CollisionDetector.cs
+++ Assets/Scripts/ScriptsHenrik/CollisionDetector.cs
@@ -4,8 +4,62 @@ using UnityEngine;
 
 public class CollisionDetector : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) 
+    public Transform grabbers;
+    private bool isGrabbing;
+    private Vector3 facingDirection;
+    [SerializeField] private float sightDistance = 5f; 
+
+    void Start() 
     {
-        Debug.Log(other.tag);    
+        isGrabbing = false;
+    }
+    
+    void Update() 
+    {
+        PlayerSight();
+    }
+    /*private void OnTriggerEnter(Collider coll) 
+    {
+        
+        Debug.Log(coll.tag);
+        if(coll.tag == "Moveable")
+        {
+            Grab(coll);
+        }
+        
+    }
+    */
+
+    private void PlayerSight()
+    {
+        /*
+        Vector3 currentPos = transform.position;
+        
+        Ray ray = new Ray(transform.position, grabbers.position);
+        RaycastHit hit;
+        Debug.DrawLine(transform.position, hit.point, Color.red);
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            Debug.Log(hit.transform.gameObject.tag);
+        }
+        
+        if (environmentInfo.collider == true && environmentInfo.collider.tag == "Moveable") 
+        {
+            
+            if (Input.GetButton("Fire1"))
+            {
+                isGrabbing = true;
+                environmentInfo.collider.gameObject.transform.parent = grabbers;
+                environmentInfo.collider.gameObject.transform.position = grabbers.position;
+                environmentInfo.collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;   
+            } else {
+                isGrabbing = false;
+                environmentInfo.collider.gameObject.transform.parent = null;
+                environmentInfo.collider.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                
+            }  
+        }
+        */
     }
 }
