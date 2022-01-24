668039ccfc1e4e5424ff390374abdb208ff4986a
Tove Backenroth <tovebac@hotmail.com>
2022-01-24 15:23:43 +0100

    code cleanup: baby event handler in place

--- Assets/Scripts/ScriptsTove/BabyController.cs
+++ Assets/Scripts/ScriptsTove/BabyController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class BabyController : MonoBehaviour
 {
     private int _timeUntilDecrease;
-
     // TODO i don't wanna hardcode this range, pls
     [Range(0, 100)] private float _babyHappiness;
     private float _happyDecreaseValue;
@@ -13,7 +12,6 @@ public class BabyController : MonoBehaviour
     [SerializeField] private GameObject _child;
 
     private BabyNeeds _currentNeed;
-    private BabyStateMachine _stateMachine;
     private MeshRenderer _meshRenderer;
     private Pointer _pointer;
     private Material _happinessBar;
@@ -36,49 +34,9 @@ public class BabyController : MonoBehaviour
         _meshRenderer = GetComponent<MeshRenderer>();
         _pointer = GetComponent<Pointer>();
         _happinessBar = _child.GetComponent<Renderer>().material;
-        _stateMachine = new BabyStateMachine(this);
 
         SetBabyValues();
         UpdateHappinessBar();
-    }
-    
-    public void OnObjectMoodEvent()
-    {
-        _stateMachine.SetObject();
-    }
-
-    public void OnWellbeingMoodEvent()
-    {
-        _stateMachine.SetWellbeing();
-    }
-
-    public void OnLoveMoodEvent()
-    {
-        _stateMachine.SetLove();
-    }
-
-    public void OnObjectComplete()
-    {
-        _stateMachine.ReturnObjectState();
-    }
-
-    public void OnWellbeingComplete()
-    {
-        _stateMachine.ReturnWellbeingState();
-    }
-
-    public void OnLoveComplete()
-    {
-        _stateMachine.ReturnLoveState();
-    }
-
-    private void OnMouseDown()
-    {
-        //debugging
-        Debug.Log("baby all good!");
-        //end of debugging
-
-        _stateMachine.ReturnObjectState();
     }
 
     private void SetBabyValues()
---
+++ Assets/Scripts/ScriptsTove/BabyEventHandler.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyEventHandler : MonoBehaviour
+{
+    private BabyController _controller;
+    private BabyStateMachine _stateMachine;
+    
+    private void Awake()
+    {
+        _controller = GetComponent<BabyController>();
+        _stateMachine = new BabyStateMachine(_controller);
+    }
+    
+    public void OnObjectMoodEvent()
+    {
+        _stateMachine.SetObject();
+    }
+
+    public void OnWellbeingMoodEvent()
+    {
+        _stateMachine.SetWellbeing();
+    }
+
+    public void OnLoveMoodEvent()
+    {
+        _stateMachine.SetLove();
+    }
+
+    public void OnObjectComplete()
+    {
+        _stateMachine.ReturnObjectState();
+    }
+
+    public void OnWellbeingComplete()
+    {
+        _stateMachine.ReturnWellbeingState();
+    }
+
+    public void OnLoveComplete()
+    {
+        _stateMachine.ReturnLoveState();
+    }
+}
